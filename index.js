a1c983c3a7806e2938dd1ab02c03dda42bc6eb2a
nftushar <nftushar5200@gmail.com>
2023-04-12 20:25:51 +0600

    JavScript Clock

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(() => {
+    da = new Date();
+    htime = da.getHours();
+    mtime = da.getMinutes();
+    stime = da.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    mysecond.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
+
